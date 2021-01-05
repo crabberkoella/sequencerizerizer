7c252bf31d80e3481e5b5a55bcfd8408f7d35370
Carver Koella <carver.koella@b-reel.com>
2021-01-05 13:10:08 -0800

    SoundToColor initial functionality exploration

---
+++ Assets/Scripts/SoundToColorControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundToColorControl : MonoBehaviour
+{
+
+    float loudness = 0;
+    Color color = new Color();
+
+    public GameObject floor;
+    Material mat;
+
+    public static List<Note> playingNotes = new List<Note>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mat = floor.GetComponent<MeshRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        loudness = 0;
+        color = new Color(0f, 0f, 0f, 1f);
+
+        foreach(Note note in playingNotes)
+        {
+            loudness += note.loudness;
+            color += note.outColor * loudness;
+        }
+        mat.SetFloat("_Loudness", loudness);
+        mat.SetColor("_Color", color);
+    }
+}
